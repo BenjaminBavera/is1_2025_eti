359852a04fe9b9b2e689dd686b8b139a66eb87fa
BenjaminBavera <benjabavera@gmail.com>
2025-10-23 11:13:26 -0300

    agrego profesor a la bd

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON; --Habilita las claves foráneas
+
 -- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
 DROP TABLE IF EXISTS users;
 
@@ -6,4 +8,9 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+);
+
+CREATE TABLE professor(
+    id INTEGER PRIMARY KEY, 
+    CONSTRAINT fk_professor_user FOREIGN KEY (id) REFERENCES users(id)
 );
